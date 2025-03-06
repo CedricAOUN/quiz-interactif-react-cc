3c5c3bc1ae4717335a5343c5a17b19268b6f416a
CedricA <cedric.j.aoun@gmail.com>
2025-03-06 14:15:30 +0100

    added base project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import FilterQuiz from './components/FilterQuiz/FilterQuiz'
 
 function App() {
 
   return (
     <>
-      
+      <h1>Quiz Interactif</h1>
+      <FilterQuiz />
     </>
   )
 }
---
+++ src/components/FilterQuiz/FilterQuiz.jsx
@@ -0,0 +1,12 @@
+const FilterQuiz = () => {
+  return (
+    <Form.Select aria-label="Default select example">
+      <option value="all">Toutes</option>
+      <option value="math">Math</option>
+      <option value="geo">Geographie</option>
+      <option value="misc">Misc.</option>
+    </Form.Select>
+  )
+}
+
+export default FilterQuiz
